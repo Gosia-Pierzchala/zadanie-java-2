89e6fbaf4a4809631535465213c4f3848bb8e8c1
Małgorzata Pierzchała <gk.kalinowska@gmail.com>
2018-07-09 13:35:26 +0200

    Zadanie-java-31 (punkt 2.)

--- src/Cars.java
+++ src/Cars.java
@@ -3,11 +3,9 @@ class Cars {
                 Car car1 = new Car("BMW", "M5", 2007, 2.6); //marka, model, rocznik, pojemność silnika
                 Car car2 = new Car("Opel", "Vectra", 2004, 1.8);
 
-                System.out.println("Samochód 1:");
-                System.out.println("Marka: " + car1.brand + ", model: " + car1.model +
-                                   ", rocznik: " + car1.year + ", pojemność: " + car1.capacity);
-                System.out.println("Samochód 2:");
-                System.out.println("Marka: " + car2.brand + ", model: " + car2.model +
-                                   ", rocznik: " + car2.year + ", pojemność: " + car2.capacity);
+                System.out.printf("%s %s", "Samochód", "1: \n");
+                System.out.printf("%s %s, %s %s, %s %d, %s %f %s", "Marka:", car1.brand, "model:", car1.model, "rocznik:", car1.year, "pojemność:", car1.capacity, "\n");
+                System.out.printf("%s %s", "Samochód", "2: \n");
+                System.out.printf("%s %s, %s %s, %s %d, %s %f %s", "Marka:", car2.brand, "model:", car2.model, "rocznik:", car2.year, "pojemność:", car2.capacity, "\n");
                 }
 }
